3fb5ba3e092ca50dbeaea4618c582d0fa0b6b095
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2025-07-08 23:52:54 +0530

    Add Prisma setup with database schema, migrations, and client integration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import { app } from "./app";
+import { PrismaClient } from "./generated/prisma";
 import "dotenv/config";
 
 const PORT = process.env.PORT || 3000;
+
+const prisma = new PrismaClient();
+
+/////// DB //////////
+// async function main() {
+  // await prisma.user.create({
+  //   data: { name: "Ravana", email: "ravanadevs@gmail.com" },
+  // });
+  // await prisma.post.create({
+  //   data: {
+  //     title: "My first post",
+  //     content: "This is my first post",
+  //     author: {
+  //       connect: {
+  //         email: "ravanadevs@gmail.com",
+  //       },
+  //     },
+  //   },
+  // });
+// }
+
+// main()
+//   .catch((e) => {
+//     console.log(e);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
